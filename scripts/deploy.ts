47c08d846653ff068879e3266783c6b5ffa26531
Ken Snyder <ken@ken.net>
2018-01-31 21:47:44 -0800

    finalizing

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,14 +5,19 @@ import * as rm from "rimraf";
 
 exec(`ts-node scripts/build.ts --color=true`, code => {
   if (code === 0) {
-    exec(`npm publish`, (publishCode: any) => {
-      if (publishCode === 0) {
-        console.log(chalk.green.bold(`- published to npm successfully 👍\n`));
-      } else {
-        console.log(
-          chalk.red.bold(`\n- problems publishing to npm: ${publishCode}  😡 `)
-        );
+    exec(
+      'yarn publish --new-version `node -p "require("./package.json").version"`',
+      (publishCode: any) => {
+        if (publishCode === 0) {
+          console.log(chalk.green.bold(`- published to npm successfully 👍\n`));
+        } else {
+          console.log(
+            chalk.red.bold(
+              `\n- problems publishing to npm: ${publishCode}  😡 `
+            )
+          );
+        }
       }
-    });
+    );
   }
 });
