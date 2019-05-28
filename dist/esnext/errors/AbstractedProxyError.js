a8e619567842f1d32c3b9086b2088982eecbdb2e
Ken Snyder <ken@ken.net>
2019-05-28 15:10:01 -0700

    fixed/improved the AbstractedProxyError class

--- dist/esnext/errors/AbstractedProxyError.js
+++ dist/esnext/errors/AbstractedProxyError.js
@@ -1,14 +1,20 @@
+import { parseStack } from "common-types";
 export class AbstractedProxyError extends Error {
-    constructor(e, errName = null, context) {
+    constructor(e, typeSubtype = null, context) {
         super(context
             ? `${e.name ? `[Proxy of ${e.name}]` : ""}` + context + ".\n" + e.message
             : `${e.name ? `[Proxy of ${e.name}]` : ""}` + e.message);
         this.stack = e.stack;
-        const name = `abstracted-firebase/${errName ? errName : e.name || "unknown-error"}`;
-        if (e.name === "Error") {
-            this.name = name;
-        }
-        this.code = name.split("/")[1];
+        const parts = typeSubtype.split("/");
+        const [type, subType] = parts.length === 2 ? parts : ["abstracted-firemodel", parts[0]];
+        this.name = `${type}/${subType}`;
+        this.code = `${subType}`;
         this.stack = e.stack;
+        try {
+            this.stackFrames = parseStack(this.stack);
+        }
+        catch (e) {
+            // ignore if there was an error parsing
+        }
     }
 }
